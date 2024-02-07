5bb273671af1e471c7682ca0619aefb7b62bd666
William Finck <144956660+filliammm@users.noreply.github.com>
2024-02-07 17:01:20 -0600

    Add files via upload

--- Database.py
+++ Database.py
@@ -6,53 +6,57 @@ class DB:
     #default constructor
     def __init__(self):
         self.filestream = None
+        self.rec_size = 0
         self.num_record = 0
         self.Id_size= 10
         self.Fname_size= 30
         self.Lname_size= 30
         self.Age_size= 10
-        self.Tnum_size= 12
+        self.Tnum_size= 20
         self.Fare_size = 10
         self.Date_size = 20
 
 
     #create database
-    def createDB(self,filename):
-        #Generate file names
+    def createDB(self, filename):
+    # Generate file names
         csv_filename = filename + ".csv"
         text_filename = filename + ".data"
+        config_filename = filename + ".config"
 
         # Read the CSV file and write into data files
         with open(csv_filename, "r") as csv_file:
-            data_list = list(csv.DictReader(csv_file,fieldnames=('ID','fname','lname','age','ticketnum','fare','date')))
+            data_list = list(csv.DictReader(csv_file, fieldnames=('ID', 'fname', 'lname', 'age', 'ticketnum', 'fare', 'date')))
 
-		# Formatting files with spaces so each field is fixed length, i.e. ID field has a fixed length of 10
+        # Formatting files with spaces so each field is fixed length, i.e., ID field has a fixed length of 10
         def writeDB(filestream, dict):
-            filestream.write("{:{width}.{width}}".format(dict["ID"],width=self.Id_size))
-            filestream.write("{:{width}.{width}}".format(dict["fname"],width=self.Fname_size))
-            filestream.write("{:{width}.{width}}".format(dict["lname"],width=self.Lname_size))
-            filestream.write("{:{width}.{width}}".format(dict["age"],width=self.Age_size))
-            filestream.write("{:{width}.{width}}".format(dict["ticketnum"],width=self.Tnum_size))
-            filestream.write("{:{width}.{width}}".format(dict["fare"],width=self.Fare_size))
-            filestream.write("{:{width}.{width}}".format(dict["date"],width=self.Date_size))
+            filestream.write("{:{width}.{width}}".format(dict["ID"], width=self.Id_size))
+            filestream.write("{:{width}.{width}}".format(dict["fname"], width=self.Fname_size))
+            filestream.write("{:{width}.{width}}".format(dict["lname"], width=self.Lname_size))
+            filestream.write("{:{width}.{width}}".format(dict["age"], width=self.Age_size))
+            filestream.write("{:{width}.{width}}".format(dict["ticketnum"], width=self.Tnum_size))
+            filestream.write("{:{width}.{width}}".format(dict["fare"], width=self.Fare_size))
+            filestream.write("{:{width}.{width}}".format(dict["date"], width=self.Date_size))
             filestream.write("\n")
 
-            #write an empty records
-            filestream.write("{:{width}.{width}}".format('_empty_',width=self.Id_size))
-            filestream.write("{:{width}.{width}}".format(' ',width=self.Fname_size))
-            filestream.write("{:{width}.{width}}".format(' ',width=self.Lname_size))
-            filestream.write("{:{width}.{width}}".format(' ',width=self.Age_size))
-            filestream.write("{:{width}.{width}}".format(' ',width=self.Tnum_size))
-            filestream.write("{:{width}.{width}}".format(' ',width=self.Fare_size))
-            filestream.write("{:{width}.{width}}".format(' ',width=self.Date_size))
+            # write an empty record
+            filestream.write("{:{width}.{width}}".format('_empty_', width=self.Id_size))
+            filestream.write("{:{width}.{width}}".format(' ', width=self.Fname_size))
+            filestream.write("{:{width}.{width}}".format(' ', width=self.Lname_size))
+            filestream.write("{:{width}.{width}}".format(' ', width=self.Age_size))
+            filestream.write("{:{width}.{width}}".format(' ', width=self.Tnum_size))
+            filestream.write("{:{width}.{width}}".format(' ', width=self.Fare_size))
+            filestream.write("{:{width}.{width}}".format(' ', width=self.Date_size))
             filestream.write("\n")
 
+        with open(config_filename, "w") as config_file:
+            config_file.write("20")
+            self.rec_size = sum([self.Id_size, self.Fname_size, self.Lname_size, self.Age_size, self.Tnum_size, self.Fare_size, self.Date_size])
+            config_file.write("72")
 
-
-        
-        with open(text_filename,"w") as outfile:
+        with open(text_filename, "w") as outfile:
             for dict in data_list:
-                writeDB(outfile,dict)
+                writeDB(outfile, dict)
 
     #read the database
     def readDB(self, filename, DBsize, rec_size):
@@ -82,10 +86,10 @@ class DB:
             fname = line[10:40]
             lname = line[40:700]
             age = line[70:80]
-            ticketnum = line[80:92]
-            fare = line[92:102]
-            date = line[102:122]
-            self.record = dict({"ID":id,"fname":fname,"lname":lname,"age":age,"ticketnum":ticketnum,fare:"fare",date:"date"})
+            ticketnum = line[80:100]
+            fare = line[100:110]
+            date = line[110:130]
+            self.record = dict({"ID": id, "fname": fname, "lname": lname, "age": age, "ticketnum": ticketnum, "fare": fare, "date": date})
     
     #open record function
     def open(self, db_name):
@@ -94,23 +98,119 @@ class DB:
         data_filename = db_name + ".data"
         try:
             with open(config_filename, 'r') as config_file:
-                self.numRecords, self.recordSize = map(int, config_file.read().splitlines())
+                self.num_record = config_file.read()
+                self.recordSize = config_file.read()
 
-            self.dataFileptr = open(data_filename, 'r+')
+            self.filestream = open(data_filename, 'r+')
             return True
         except FileNotFoundError:
             return False
+    #is open record method
+    def isOpen(self):
+        # Check if the database is open
+        return self.filestream is not None
     
-    #close record function
+    #close record method
     def close(self):
         # Implement close method to close data file
-        if self.dataFileptr is not None:
-            self.dataFileptr.close()
-            self.numRecords = 0
+        if self.filestream is not None:
+            self.filestream.close()
+            self.num_record = 0
             self.recordSize = 0
-            self.dataFileptr = None
-            
+            self.filestream = None
     
+    #display record method        
+    def readRecord(self, recordNum, passengerId, fname, lname, age, ticketNum, fare, date):
+        # Implement readRecord method to read a record from the data file
+        if self.isOpen():
+            if 0 <= recordNum < self.num_record:
+                # Move the file pointer to the beginning of recordNum
+                self.filestream.seek(recordNum * self.recordSize)
+                
+                # Read the key from the record
+                key = self.filestream.read(self.Id_size).strip()
+
+                if key == '_empty_':
+                    # Empty record
+                    return 0
+                else:
+                    # Non-empty record, read the rest of the fields
+                    passengerId = key
+                    fname = self.filestream.read(self.Fname_size).strip()
+                    lname = self.filestream.read(self.Lname_size).strip()
+                    age = self.filestream.read(self.Age_size).strip()
+                    ticketNum = self.filestream.read(self.Tnum_size).strip()
+                    fare = self.filestream.read(self.Fare_size).strip()
+                    date = self.filestream.read(self.Date_size).strip()
+
+                    return 1  # Successful read
+            else:
+                print("Invalid record number.")
+                return -1  # Invalid record number
+        else:
+            print("Database is not open.")
+            return -1  # Database is not open
+     
+    #write Record method           
+    def writeRecord(self, recordNum, passengerId, fname, lname, age, ticketNum, fare, date):
+        # Implement writeRecord method to write a record to the data file
+        if self.filestream is not None:
+            if 0 <= recordNum < self.num_record:
+                # Move the file pointer to the beginning of the record
+                self.filestream.seek(recordNum * self.recordSize)
+
+                # Format the record data (assuming a fixed format)
+                record_data = f"{passengerId},{fname},{lname},{age},{ticketNum},{fare},{date}"
+
+                # Pad the record data if needed to match recordSize
+                record_data = record_data.ljust(self.recordSize, '\0')
+
+                # Write the record to the file
+                self.filestream.write(record_data)
+
+                print(f"Record {recordNum} updated successfully.")
+                return 1  # 1 indicates successful update
+            else:
+                print("Invalid record number.")
+                return -1  # -1 indicates invalid record number
+    
+    #update Record method        
+    def updateRecord(self, passengerId, fname, lname, age, ticketNum, fare, date):
+        # Implement updateRecord method
+        if self.isOpen():
+            recordNum, _, _, _, _, _, _, _ = self.binarySearch(passengerId)
+            if recordNum != -1:
+                # Call writeRecord to update the existing record
+                return self.writeRecord(recordNum, passengerId, fname, lname, age, ticketNum, fare, date)
+            else:
+                print(f"Record with Passenger ID {passengerId} not found.")
+                return False
+
+    #add Record method
+    def addRecord(self, passengerId, fname, lname, age, ticketNum, fare, date):
+        # Implement addRecord method
+        if self.isOpen():
+            # Search for an empty record
+            recordNum, _, _, _, _, _, _, _ = self.binarySearch('\0')
+            if recordNum != -1:
+                # Call writeRecord to add the new record to an empty slot
+                return self.writeRecord(recordNum, passengerId, fname, lname, age, ticketNum, fare, date)
+            else:
+                print("No empty record found. Trying to extend the file.")
+                # You can implement the logic to extend the file and add the new record here
+                # ...
+
+    #delete Record method
+    def deleteRecord(self, passengerId):
+        # Implement deleteRecord method
+        if self.isOpen():
+            recordNum, _, _, _, _, _, _, _ = self.binarySearch(passengerId)
+            if recordNum != -1:
+                # Call writeRecord to delete the record by overwriting with default values
+                return self.writeRecord(recordNum, '\0', '', '', '', '', '', '')
+            else:
+                print(f"Record with Passenger ID {passengerId} not found.")
+                return False
     
     #Binary Search by record id
     def binarySearch(self, input_ID):
@@ -160,9 +260,6 @@ class DB:
 
         return found
 
-
-    
-
     def findNearestNonEmpty(self, start, low_limit, high_limit):
         step = 1  # Initialize step size
 
@@ -189,9 +286,6 @@ class DB:
                 break
 
         return -1  # No non-empty record found
-
-
-
 
     #close the database
     def CloseDB(self):
--- TitanicTestDB.py
+++ TitanicTestDB.py
@@ -1,6 +1,6 @@
 from Database import DB
 
-filepath = "SmallTitanic"
+filepath = None
 DBsize = 20
 rec_size = 72
 
@@ -12,42 +12,53 @@ while True:
     print('1. Create new DB')
     print('2. Open DB')
     print('3. Close DB')
-    print('4. Display Record')
-    print('5. Update Record')
+    print('4. Read Record')
+    print('5. Display Record')
     print('6. Create Report')
-    print('7. Add Record')
+    print('7. Update Record')
     print('8. Delete Record')
-    print('9. Quit')
+    print('9. Add Record')
     print('\n')
-    key = input('Please select an option 1-5: ').strip()
+    key = input('Please select an option 1-9: ').strip()
     if key == '1':
         #Create new DB
-        break
+        filepath = input("Please input the name of the csv file you wish to create a DB with: ")
+        Titanic.createDB(filepath)
+        continue
     elif key == '2':
         #Open DB
-        break
+        openDB = input("Please input the name of the file you wish to open a DB with: ")
+        Titanic.open(openDB)
+        print("The database has been opened\n")
+        continue
     elif key == '3':
         #Close DB
-        break
+        Titanic.close()
+        continue
     elif key == '4':
-        #Display Record
-        break
+        #Read Record
+        
+        continue
     elif key == '5':
-        #Update Record
-        break
+        #Display Record
+        
+        continue
     elif key == '6':
         #Create Report
-        break
+        
+        continue
     elif key == '7':
-        #Add Record
-        break
+        #Update Record
+        
+        continue
     elif key == '8':
         #Delete Record
-        break
+        cin = input("Please indicate the ID of the record you wish to remove: ")
+        Titanic.deleteRecord(cin)
+        continue
     elif key == '9':
-        #Quit
-        Titanic.CloseDB()
-        break
+        #Add Record
+        Titanic.addRecord()
     else:
         print('wrong input please try again')
         continue
